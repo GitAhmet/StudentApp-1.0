93ed9fac3d5376330e35169c69e062344ed4b659
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-12-07 12:45:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentAppContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("StudentAppContext") ?? throw new InvalidOperationException("Connection string 'StudentAppContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API");
+    s.RoutePrefix = "api";
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
